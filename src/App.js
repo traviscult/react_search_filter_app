51d23321fad69b11cb17186ea72261040ea56e58
traviscult <60018617+traviscult@users.noreply.github.com>
2023-02-03 18:17:39 -0600

    created input and displayed users first names

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import "./App.css";
+import JSONDATA from "./MOCK_DATA.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Search..."></input>
+      {JSONDATA.map((val, key) => {
+        return (
+          <div className="firstName" key={key}>
+            {" "}
+            <p>{val.first_name}</p>{" "}
+          </div>
+        );
+      })}
     </div>
   );
 }
